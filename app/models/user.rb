51868ef264a94b7802fe9b32233f012edb0715f3
jjenkins120 <jjenkins120@gmail.com>
2020-09-08 13:41:04 -0500

    resolved db issues, added relationship between user and comment, added relationship between post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :destinations, through: :trips
     has_many :posts, through: :trips
     has_many :comments
-    has_many :commented_on_posts, through: :comments
+    #has_many :commented_on_posts, through: :comments, foreign_key: :user_id, class_name: "User"
     has_secure_password
 
 end
